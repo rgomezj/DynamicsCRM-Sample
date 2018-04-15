d192096b3a6c3f11114e88d6f5fcd9e1468b7f90
Roger Gomez Jaramillo <rogergomez780@hotmail.com>
2018-04-15 17:04:26 -0500

    Commit for the sample plugin that shows tracing and context information

---
+++ DynamicsCRMSample/DynamicsCRMSample/ContactCreatedHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicsCRMSample
+{
+    public class ContactCreatedHandler : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            //Extract the tracing service for use in debugging sandboxed plug-ins.
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Obtain the target entity from the input parameters.
+            Entity entity = (Entity)context.InputParameters["Target"];
+            StringBuilder builder = new StringBuilder();
+            foreach (KeyValuePair<String, Object> attribute in entity.Attributes)
+            {
+                builder.AppendLine(attribute.Key + ": " + attribute.Value);
+            }
+
+            tracingService.Trace("Entity attributes: {0}", builder.ToString());
+        }
+    }
+}
